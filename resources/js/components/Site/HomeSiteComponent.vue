bf1a68b6ed057d0626f451dc03f6ad1b92f5d63b
Giordani da Silveira dos Santos <dani.santossilveira@hotmail.com>
2023-06-20 20:55:08 -0300

    ajuste src img

--- resources/js/components/Site/HomeSiteComponent.vue
+++ resources/js/components/Site/HomeSiteComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container-fluid">
         <div class="img-container">
-            <img src="http://127.0.0.1:8000/images/chopeiras.jpg" alt="" style="width: 35%;height: 100%;">
+            <img src="https://hornetbier.com.br/images/chopeiras.jpg" alt="" style="width: 35%;height: 100%;">
             <br><br><h1>Site em Construção</h1>
         </div>
     </div>
