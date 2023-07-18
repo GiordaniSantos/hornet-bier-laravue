db2c73adf4e48238d533d2eefc07ded8de54280c
Giordani da Silveira dos Santos <dani.santossilveira@hotmail.com>
2023-07-17 22:31:06 -0300

    ajustes contato

--- resources/js/components/Site/ContatoSite.vue
+++ resources/js/components/Site/ContatoSite.vue
@@ -13,14 +13,17 @@
             <!-- Contact Section Form-->
             <div class="row justify-content-center">
                 <div class="col-lg-8 col-xl-7">
-                    <p class="lead" style="font-size: 0.95rem;text-align: center;">Whatsapp(51) <a href="https://web.whatsapp.com/send?phone=5551999446655&text=Olá+é+da+Hornet+Bier?" target="_blank" style="text-decoration: none;color: #8e1b1b;">99944-5544</a><br><br>
-                        Telefone(51) 99944-5544<br><br>
-                        E-mail <a href="mailto:contato@hornetbier.com.br?subject=Assunto do email&body=Olá" style="text-decoration: none;color: #8e1b1b;">contato@hornetbier.com.br</a><br><br>
-                        Horário de Atendimento Segunda à sexta das 08h às 17h50<br><br>
-                        Hornet Bier<br>
-                        CNPJ: 03.670.744/0001-01<br>
-                        Sapucaia do Sul/RS
-                    </p>
+                    <div style="text-align: center;">
+                        <h4>Fone/Whatsapp: <a href="https://web.whatsapp.com/send?phone=5551999446655&text=Olá+é+da+Hornet+Bier?" target="_blank" style="text-decoration: none;color: #8e1b1b;">(51) 99944-6655</a></h4><br><br>
+                        <h4>Fone/Whatsapp: <a href="https://web.whatsapp.com/send?phone=5551991052458&text=Olá+é+da+Hornet+Bier?" target="_blank" style="text-decoration: none;color: #8e1b1b;">(51) 99105-2458</a></h4><br><br>
+                        <h4>E-mail: <a href="mailto:contato@hornetbier.com.br?subject=Assunto do email&body=Olá" style="text-decoration: none;color: #8e1b1b;">contato@hornetbier.com.br</a></h4><br><br>
+                        <h4>Horário de Atendimento<br>
+                            Segunda à sexta das 09:00h às 21:00h<br>
+                            Sábado das 09:00h às 19:00h</h4><br><br>
+                        <h4>Hornetbier Chopeiras e Manutenção<br>
+                        CNPJ: 50.934.088/0001-97<br>
+                        Sapucaia do Sul/RS</h4>
+                    </div>
                 </div>
             </div>
         </div>
