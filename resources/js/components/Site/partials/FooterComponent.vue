a164cc8f16f045fd1e42f4c76d5a32c6f3220c71
Giordani da Silveira dos Santos <dani.santossilveira@hotmail.com>
2023-07-19 10:00:02 -0300

    ajuste texto modal assepsia

--- resources/js/components/Site/partials/FooterComponent.vue
+++ resources/js/components/Site/partials/FooterComponent.vue
@@ -169,7 +169,7 @@
                                     <div class="divider-custom-line"></div>
                                 </div>
                                 <img class="img-fluid rounded mb-5" src="images/assepsia.jpg" alt="..." />
-                                <p class="mb-4" style="margin-bottom: 4.5rem !important;">Assepsia da Chopeira é um processo de limpeza interna da serpentina, por onde passa o chopp, com auxilio de uma máquina que faz a recirculação dos líquidos peracéticos. Essa ação tem por função eliminar todos os organismos que ficam armazenados durante o uso do equipamento. Oferecemos o serviço para sua chopeira. </p>
+                                <p class="mb-4" style="margin-bottom: 4.5rem !important;">Assepsia da Chopeira é um processo de limpeza interna da serpentina, por onde passa o chopp, com auxilio de uma máquina que faz a recirculação dos líquidos peracéticos. Essa ação tem por função eliminar todos os organismos que ficam armazenados durante o uso do equipamento. Agende o serviço para sua chopeira. </p>
                                 <!-- START Widget WhastApp -->
                                 <div class="text-center">
                                     <a href="https://api.whatsapp.com/send?phone=5551999446655&text=Olá!%20Gostaria%20de%20mais%20informações%20sobre%20esse%20serviço" class="bt-whatsApp" target="_blank" style="right:39%; position: absolute; width:60px;height:60px;bottom:40px;z-index:100;">
